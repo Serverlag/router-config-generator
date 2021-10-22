d2dedef51f5c76ea66a2c7b37cf505dc6a4c8795
Serverlag <35069676+Serverlag@users.noreply.github.com>
2021-10-22 23:07:49 +0930

    build jinja template and pass it form variables

--- router-config-generator/router-config-generator/views.py
+++ router-config-generator/router-config-generator/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, config, redirect, render_template, url_for
+from flask import Flask, config, render_template
 from . import app
 from .forms import configForm
 
@@ -7,7 +7,24 @@ from .forms import configForm
 def home():
     form = configForm()
     if form.validate_on_submit():
-        return redirect(url_for("success"))
+        return render_template(
+            "generate.html",
+            form=form,
+            rtrHostname = form.rtrHostname.data,
+            dnsSuffix = form.dnsSuffix.data,
+            tzName = form.tzName.data,
+            tzOffset = form.tzOffset.data,
+            dnsResolverPrimary = form.dnsResolverPrimary.data,
+            dnsResolverSecondary = form.dnsResolverSecondary.data,
+            rtrAdminUsername = form.rtrAdminUsername.data,
+            rtrAdminPassword = form.rtrAdminPassword.data,
+            lanIPAddress = form.lanIPAddress.data,
+            lanCidrPrefixSize = form.lanCidrPrefixSize.data,
+            wanTechnologyType = form.wanTechnologyType.data,
+            wanAuthType = form.wanAuthType.data,
+            rtrVendor = form.rtrVendor.data,
+            rtrSeries = form.rtrSeries.data
+        )
     return render_template(
         "home.html",
         form=form,
